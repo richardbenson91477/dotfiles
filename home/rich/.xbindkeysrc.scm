a0558d3acf606d927522e317c4bf807cb6e3e9f1
Richard Benson <richardbenson91477@protonmail.com>
2021-08-22 01:00:44 -0500

    left and right mouse wheel click change audio volume

--- home/rich/.xbindkeysrc.scm
+++ home/rich/.xbindkeysrc.scm
@@ -1,4 +1,5 @@
 (xbindkey '("b:6") "xdotool key --delay 400 Super+x")
 ;;(xbindkey '(release "b:6") "xdotool keyup Super
 (xbindkey '("b:7") "xdotool key --delay 400 Super+z")
-
+(xbindkey '("b:8") "amixer sset Master 5%-")
+(xbindkey '("b:9") "amixer sset Master 5%+")
